b7d17eeb3ec376307f69e9865e01a414cbe8775b
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-05-20 16:35:43 -0300

    Ejercicio 1 Terminado

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero;
+string textoNumero;
+bool resultado;
+
+do
+{
+    Console.WriteLine("Ingrese un número mayor a 0");
+    textoNumero = Console.ReadLine();
+
+    resultado = int.TryParse(textoNumero, out numero);
+
+    if (resultado)
+    {
+        Console.WriteLine("\nEl texto ingresado es un numero");
+    }else
+    {
+        Console.WriteLine("\nEl texto ingresado NO es un numero\n");
+    }
+
+} while (!resultado);
+ 
+if (numero > 0)
+{
+    Console.WriteLine("El numero ingresado es mayor a 0");
+}else
+{
+    Console.WriteLine("El numero ingresado es menor o igual 0");
+    
+}
