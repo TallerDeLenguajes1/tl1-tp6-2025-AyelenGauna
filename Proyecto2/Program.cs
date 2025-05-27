1bb273d3f8aca2fb6a25878e981dbb8136f81d29
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-05-27 12:42:37 -0300

    CalculadoraV1 simple

---
+++ Proyecto2/Program.cs
@@ -0,0 +1,13 @@
+﻿string opcion;
+int numero;
+Console.WriteLine("*** CALCULADORA SIMPLE ***\nOpciones:\n1)Suma  2)Resta  3)Multiplicación  4)División");
+opcion = Console.ReadLine();
+
+if (int.TryParse(opcion, out numero) && (numero > 0 && 5 > numero))
+{
+    Console.WriteLine("El dato ingresado es un número");
+    
+}else
+{
+    Console.WriteLine("El dato ingresado no es un número o no es una opción válida");
+}
