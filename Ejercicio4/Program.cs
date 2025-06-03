84a398cc4abb543871941b56b88da746203b38b7
Dana Ayelen Gauna <ayelengauna564gmail.com>
2025-06-03 15:18:29 -0300

    Inicio del Ejercicio 4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,12 @@
+﻿string texto1, texto2, cadenaTextos;
+int longitud1;
+
+Console.WriteLine("Ingrese el primer texto");
+texto1 = Console.ReadLine();
+longitud1 = texto1.Length;
+Console.WriteLine($"\nLongitud del primer texto: {longitud1}\n");
+
+Console.WriteLine("Ingrese el segundo texto");
+texto2 = Console.ReadLine();
+cadenaTextos = string.Concat(texto1, texto2);
+Console.WriteLine($"\nConcatenación del primer y segundo texto: {cadenaTextos}\n");
