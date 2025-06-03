187e2a8840b22e91ca668d2395674f02477fe1f6
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-06-03 00:29:30 -0300

    Interfaz de la calculadora mejorada para que el usuario decida que operación matematica realizar

--- Proyecto2/Program.cs
+++ Proyecto2/Program.cs
@@ -1,53 +1,68 @@
 ﻿string opcion;
 int numero;
-Console.WriteLine("*** CALCULADORA SIMPLE ***");
+Console.WriteLine("*** CALCULADORA ***");
 
 do
 {
     do
     {
-        Console.WriteLine("\nOpciones:\n1)Suma  2)Resta  3)Multiplicación  4)División");
+        Console.WriteLine("\nOpciones:\n5)Valor Absoluto  6)Cuadrado de un número  7)Raíz cuadrada de un número mayor/igual a 0  8)Seno  9)Coseno  10)Parte entera de un tipo float");
         opcion = Console.ReadLine();
 
-        if (int.TryParse(opcion, out numero) && (numero > 0 && 5 > numero))
+        if (int.TryParse(opcion, out numero) && (numero > 4 && 11 > numero))
         {
-            string num1S, num2S;
-            int num1N, num2N;
+            string numS;
+            double numN;
 
-            Console.WriteLine("\nNúmero 1: ");
-            num1S = Console.ReadLine();
-            int.TryParse(num1S, out num1N);
-
-            Console.WriteLine("\nNúmero 2: ");
-            num2S = Console.ReadLine();
-            int.TryParse(num2S, out num2N);
-
-            switch (numero)
+            do
             {
-                case 1:
-                    int suma = num1N + num2N;
-                    Console.WriteLine($"** SUMA **\n{num1S} + {num2S} = {suma}");
-                    break;
-                case 2:
-                    int resta = num1N - num2N;
-                    Console.WriteLine($"** RESTA **\n{num1S} - {num2S} = {resta}");
-                    break;
-                case 3:
-                    int multiplicación = num1N * num2N;
-                    Console.WriteLine($"** MULTIPLICACIÓN **\n{num1S} x {num2S} = {multiplicación}");
-                    break;
-                case 4:
-                    if (num2N == 0)
+                Console.WriteLine("\nNúmero:");
+                numS = Console.ReadLine();
+
+                if (double.TryParse(numS, out numN))
+                {
+                    switch (numero)
                     {
-                        Console.WriteLine("No se puede dividir sobre 0");
+                        case 5:
+                            double vAbs = Math.Abs(numN);
+                            Console.WriteLine($"|{numN}| = {vAbs}");
+                            break;
+                        case 6:
+                            double pow = Math.Pow(numN, 2);
+                            Console.WriteLine($"{numN}^2 = {pow}");
+                            break;
+                        case 7:
+                            if (numN >= 0)
+                            {
+                                double raiz = Math.Sqrt(numN);
+                                Console.WriteLine($"Sqrt({numN}) = {raiz}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se puede calcular la raíz de indice par a un número negativo");
+                            }
+                            break;
+                        case 8:
+                            double sen = Math.Sin(numN);
+                            Console.WriteLine($"Sen({numN}) = {sen}");
+                            break;
+                        case 9:
+                            double cos = Math.Cos(numN);
+                            Console.WriteLine($"Cos({numN}) = {cos}");
+                            break;
+                        case 10:
+                            double entero = Math.Truncate(numN);
+                            Console.WriteLine($"Parte entera del decimal {numN} = {entero}");
+                            break;
                     }
-                    else
-                    {
-                        double división = (double)num1N / num2N;
-                        Console.WriteLine($"** DIVISIÓN **\n{num1S} / {num2S} = {división}");
-                    }
-                    break;
-            }
+
+                }
+                else
+                {
+                    Console.WriteLine("El dato ingresado no es válido");
+                }
+
+            } while (!double.TryParse(numS, out numN));
 
         }
         else
@@ -55,7 +70,7 @@ do
             Console.WriteLine("El dato ingresado no es un número o no es una opción válida\n");
         }
 
-    } while (!(int.TryParse(opcion, out numero)) || (numero < 1 || 4 < numero));
+    } while (!(int.TryParse(opcion, out numero)) || (numero < 5 || 10 < numero));
 
     do
     {
@@ -67,12 +82,8 @@ do
             Console.WriteLine("La opción o el dato ingresado es inválido");
         }
 
-        if (numero == 0)
-        {
-            Console.WriteLine("\n¡¡¡Hasta luego!!!");
-        }
-
     } while (!int.TryParse(opcion, out numero) || (numero != 0 && numero != 1));
 
 } while (!int.TryParse(opcion, out numero) || numero == 1);
+
 
