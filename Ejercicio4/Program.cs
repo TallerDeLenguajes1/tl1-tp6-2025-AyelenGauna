89521f9a40527978ca58918b6054ec9843b5c3f9
Dana Ayelen Gauna <ayelengauna564gmail.com>
2025-06-03 15:31:31 -0300

    Dos cadenas ingresadas por el usuario son concatendas. Luego se obtiene una subcadena de cada cadena

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -1,4 +1,4 @@
-﻿string texto1, texto2, cadenaTextos;
+﻿string texto1, texto2, cadenaTextos, subcadena1, subcadena2;
 int longitud1;
 
 Console.WriteLine("Ingrese el primer texto");
@@ -10,3 +10,9 @@ Console.WriteLine("Ingrese el segundo texto");
 texto2 = Console.ReadLine();
 cadenaTextos = string.Concat(texto1, texto2);
 Console.WriteLine($"\nConcatenación del primer y segundo texto: {cadenaTextos}\n");
+
+subcadena1 = texto1.Substring(5);
+Console.WriteLine($"\nSubcadena del primer texto: {subcadena1}\n");
+
+subcadena2 = texto2.Substring(5);
+Console.WriteLine($"\nSubcadena del segundo texto: {subcadena2}\n");
