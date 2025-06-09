8b9b67eda1725f93d6ae475b1d3107358ae684fc
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-06-08 23:10:20 -0300

    Ocurrencias de una palabra en el primer texto ingresado

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -23,64 +23,84 @@ string opcion;
 int numero;
 Console.WriteLine("*** CALCULADORA SIMPLE ***");
 
-do
-{
     do
     {
         Console.WriteLine("\nOpciones:\n1)Suma  2)Resta  3)Multiplicación  4)División");
         opcion = Console.ReadLine();
 
-        if (int.TryParse(opcion, out numero) && (numero > 0 && 5 > numero))
+    if (int.TryParse(opcion, out numero) && (numero > 0 && 5 > numero))
+    {
+        string mensaje = "";
+        int num1N, num2N, resultado;
+
+        Console.WriteLine("\nNúmero 1: ");
+        num1N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nNúmero 2: ");
+        num2N = int.Parse(Console.ReadLine());
+
+        switch (numero)
         {
-            string num1S, num2S, mensaje = "";
-            int num1N, num2N, resultado;
-
-            Console.WriteLine("\nNúmero 1: ");
-            num1S = Console.ReadLine();
-            int.TryParse(num1S, out num1N);
-
-            Console.WriteLine("\nNúmero 2: ");
-            num2S = Console.ReadLine();
-            int.TryParse(num2S, out num2N);
-
-            switch (numero)
-            {
-                case 1:
-                    resultado = num1N + num2N;
-                    mensaje = $"La suma de {num1S} y {num2S} es igual a: {resultado.ToString()}";
-                    break;
-                case 2:
-                    resultado = num1N - num2N;
-                    mensaje = $"La resta de {num1S} y {num2S} es igual a: {resultado.ToString()}";
-                    break;
-                case 3:
-                    resultado = num1N * num2N;
-                    mensaje = $"La multiplicación de {num1S} y {num2S} es igual a: {resultado.ToString()}";
-                    break;
-                case 4:
-                    if (num2N == 0)
-                    {
-                        Console.WriteLine("No se puede dividir sobre 0");
-                    }
-                    else
-                    {
-                        resultado = num1N / num2N;
-                        mensaje = $"La división de {num1S} y {num2S} es igual a: {resultado.ToString()}";
-                    }
-                    break;
-            }
-
-            if (mensaje != "")
-            {
-                Console.WriteLine($"\n{mensaje}");
-            }
-
+            case 1:
+                resultado = num1N + num2N;
+                mensaje = $"La suma de {num1N} y {num2N} es igual a: {resultado}";
+                break;
+            case 2:
+                resultado = num1N - num2N;
+                mensaje = $"La resta de {num1N} y {num2N} es igual a: {resultado}";
+                break;
+            case 3:
+                resultado = num1N * num2N;
+                mensaje = $"La multiplicación de {num1N} y {num2N} es igual a: {resultado}";
+                break;
+            case 4:
+                if (num2N == 0)
+                {
+                    Console.WriteLine("No se puede dividir sobre 0");
+                }
+                else
+                {
+                    resultado = num1N / num2N;
+                    mensaje = $"La división de {num1N} y {num2N} es igual a: {resultado}";
+                }
+                break;
         }
-        else
+
+        if (mensaje != "")
         {
-            Console.WriteLine("El dato ingresado no es un número o no es una opción válida\n");
-        }
+            Console.WriteLine($"\n{mensaje}\n");
+            Console.WriteLine($"*** MENSAJE DEL RESULTADO DE LA OPERACIÓN MATEMÁTICA IMPRESO CARACTER POR CARACTER ***\n");
+
+            foreach (var letra in mensaje)
+            {
+                Console.WriteLine(letra);
+            }
+        }   
+
+    }
+    else
+    {
+        Console.WriteLine("El dato ingresado no es un número o no es una opción válida\n");
+    }
 
     } while (!(int.TryParse(opcion, out numero)) || (numero < 1 || 4 < numero));
 
-} while (!int.TryParse(opcion, out numero));
+    Console.WriteLine("\n*** OCURRENCIAS DE UNA PALABRA ***\n");
+    Console.Write("Ingresá la palabra que querés buscar del primer texto: ");
+    string palabra = Console.ReadLine();
+    int ocurrencia = 0, posicion = 0;
+
+    while ((posicion = texto1.IndexOf(palabra, posicion, StringComparison.OrdinalIgnoreCase)) != -1)
+    {
+        ocurrencia++;
+        posicion += palabra.Length;
+    }
+
+    if (ocurrencia != 0)
+    {
+        Console.WriteLine($"\n'{palabra}' aparece {ocurrencia} vez/veces\n");
+    }
+    else
+    {
+        Console.WriteLine($"\n'{palabra}' no aparece en el primer texto\n");
+    }
